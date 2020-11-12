7ad26ef18475627f0ae0818d3c6c712e59b7dc4e
Sasha Sinitsyn <39600558+AlexandrSinitsyn@users.noreply.github.com>
2020-11-12 18:31:03 +0300

    sudoku_update

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,12 +1,22 @@
-import math
 import pathlib
-from tokenize import group
 import typing as tp
+
 from math import sqrt
+
+import random
 
 max_number = 9
 
 count_in_block = int(sqrt(max_number))
+
+T = tp.TypeVar("T")
+
+
+def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
+    path = pathlib.Path(path)
+    with path.open() as f:
+        puzzle = f.read()
+    return create_grid(puzzle)
 
 
 def create_grid(puzzle: str) -> tp.List[tp.List[str]]:
@@ -17,21 +27,39 @@ def create_grid(puzzle: str) -> tp.List[tp.List[str]]:
     while puzzle[max_number] != '\n':
         max_number += 1
 
-    count_in_block = int(math.sqrt(max_number))
+    count_in_block = int(sqrt(max_number))
 
     digits = [c for c in puzzle if c in ([str(i) for i in range(1, max_number + 1)] + ["."])]
     grid = group(digits, max_number)
     return grid
 
 
-def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
-    path = pathlib.Path(path)
-    with path.open() as f:
-        puzzle = f.read()
-    return create_grid(puzzle)
+tab_count = 5
 
 
-T = tp.TypeVar("T")
+def display(grid):
+    tab = ""
+    for i in range(tab_count):
+        tab += "\t"
+
+    for row in range(len(grid)):
+        print(tab, end="")
+        for e in range(len(grid[row])):
+            print(grid[row][e], end=" ")
+
+            if (e + 1) % count_in_block == 0 and (e + 1) != len(grid[row]):
+                print("|", end=" ")
+        print()
+
+        if (row + 1) % count_in_block == 0 and (row + 1) != len(grid):
+            print(tab, end="")
+            for i in range(count_in_block):
+                for j in range(count_in_block * 2):
+                    print("-", end="")
+                if i != count_in_block - 1:
+                    print("+", end="")
+                else:
+                    print()
 
 
 def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
@@ -92,61 +120,6 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
             for j in range(h * count_in_block, h * count_in_block + count_in_block)]
 
 
-def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
-    """ Поиск решения для указанного пазла.
-
-    Как решать Судоку?
-    1. Найти свободную позицию
-    2. Найти все возможные значения, которые могут находиться на этой позиции
-    3. Для каждого возможного значения:
-        3.1. Поместить это значение на эту позицию
-        3.2. Продолжить решать оставшуюся часть пазла
-
-    >>> grid = read_sudoku('puzzle1.txt')
-    >>> solve(grid)
-    [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
-    """
-
-    """
-    def Перебор(Ситуация):
-    if Ситуация конечная:
-        Завершающая обработка
-    else:
-        for Действие in Множество всех возможных действий:
-            Применить Действие к Ситуация
-            Перебор(Ситуация)
-            откатить Действие назад
-    """
-
-    """
-    i, j = find_empty_positions(grid)
-
-    if i == -1:
-        return True
-    for e in range(1, 10):
-        if isValid(grid, i, j, e):
-            grid[i][j] = e
-            if solve(grid, i, j):
-                return True
-            # Undo the current cell for backtracking
-            grid[i][j] = 0
-    return False"""
-
-    def check(matrix):
-        pos = find_empty_positions(matrix)
-        if pos == (-1, -1):
-            return matrix, True
-        else:
-            for e in find_possible_values(matrix, pos):
-                matrix[pos[0]][pos[1]] = e
-                if check(matrix):
-                    return matrix, True
-                matrix[pos[0]][pos[1]] = "."
-            return None, False
-
-    return check(grid)[0]
-
-
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
     """ Найти первую свободную позицию в пазле
 
@@ -183,6 +156,36 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     return values ^ possible_values
 
 
+def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
+    """ Решение пазла, заданного в grid """
+    """ Как решать Судоку?
+        1. Найти свободную позицию
+        2. Найти все возможные значения, которые могут находиться на этой позиции
+        3. Для каждого возможного значения:
+            3.1. Поместить это значение на эту позицию
+            3.2. Продолжить решать оставшуюся часть пазла
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> solve(grid)
+    [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
+    """
+
+    def check(matrix):
+        pos = find_empty_positions(matrix)
+        if pos == (-1, -1):
+            return matrix, True
+        else:
+            for e in find_possible_values(matrix, pos):
+                matrix[pos[0]][pos[1]] = e
+
+                if check(matrix)[1]:
+                    return matrix, True
+
+                matrix[pos[0]][pos[1]] = "."
+            return None, False
+
+    return check(grid)[0]
+
+
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     col_indices = [i for i in range(count_in_block)]
@@ -197,79 +200,48 @@ def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     return all(passed)
 
 
-tab_count = 5
+def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
+    """Генерация судоку заполненного на N элементов
+    >>> grid = generate_sudoku(40)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    41
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(1000)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    0
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(0)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    81
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    """
+    matrix = [['.' for _ in range(max_number)] for _ in range(max_number)]
+    matrix = solve(matrix)
+
+    count = 0
+    while count < max_number * max_number - N:
+        i = random.randint(0, max_number - 1)
+        j = random.randint(0, max_number - 1)
+
+        if matrix[i][j] != '.':
+            matrix[i][j] = '.'
+            count += 1
+
+    return matrix
 
 
-def display(grid):
-    tab = ""
-    for i in range(tab_count):
-        tab += "\t"
-
-    for row in range(len(grid)):
-        print(tab, end="")
-        for e in range(len(grid[row])):
-            print(grid[row][e], end=" ")
-
-            if (e + 1) % count_in_block == 0 and (e + 1) != len(grid[row]):
-                print("|", end=" ")
-        print()
-
-        if (row + 1) % count_in_block == 0 and (row + 1) != len(grid):
-            print(tab, end="")
-            for i in range(count_in_block):
-                for j in range(count_in_block * 2):
-                    print("-", end="")
-                if i != count_in_block - 1:
-                    print("+", end="")
-                else:
-                    print()
-
-
-full_length = 0
-
-
-def separator():
-    global full_length
-
-    for i in range(5 * 4):
-        print("-", end="")
-
-        full_length += 1
-    for i in range((count_in_block * 2 + 1) * count_in_block - 1):
-        print("-", end="")
-
-        full_length += 1
-    for i in range(5 * 4):
-        print("-", end="")
-
-        full_length += 1
-    print()
-
-
-print()
-sudoku = read_sudoku('puzzle1.txt')
-display(sudoku)
-
-print()
-separator()
-
-for _ in range((full_length - len("SOLVING")) // 2 + 1):
-    print(" ", end="")
-print("SOLVING", end="")
-for _ in range((full_length - len("SOLVING")) // 2):
-    print(" ", end="")
-print()
-
-separator()
-print()
-
-SOLUTION = solve(sudoku)
-
-if check_solution(SOLUTION):
-    display(SOLUTION)
-else:
-    print("Your sudoku can not be solved!")
-
-print()
-
-
+if __name__ == "__main__":
+    for fname in ["puzzle1.txt", "puzzle2.txt", "puzzle3.txt"]:
+        grid = read_sudoku(fname)
+        display(grid)
+        solution = solve(grid)
+        if not solution:
+            print(f"Puzzle {fname} can't be solved")
+        else:
+            display(solution)
